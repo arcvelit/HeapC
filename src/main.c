c3f4c81b1d9fad4b64d83675a8e126410923ec67
arcvelit <louka.f.s@gmail.com>
2023-12-17 17:18:48 -0500

    Heap allocates data

--- src/main.c
+++ src/main.c
@@ -1,41 +1,58 @@
 #include <stdio.h>
 
 
-#define HEAP_CAPACITY 10
+#define HEAP_CAP 1000
+#define MEM_BLOCK_CAP 10
 typedef unsigned char byte;
+typedef struct {
+    byte *ptr;
+    size_t size;
+} Mem_Block;
 
-byte heap[HEAP_CAPACITY] = {0}; 
+size_t heap_size = 0;
 
+Mem_Block heap_blocks[MEM_BLOCK_CAP] = {0};
+byte heap[HEAP_CAP] = {0}; 
 
-// Dump heap contents to console
-void heap_dump() {
-    for (int i = 0; i < HEAP_CAPACITY; i++) {
-        printf("%d", heap[i]);
-    }
-}
+byte *heap_ptr = heap;
 
 void *heapc_alloc(size_t size) {
+
+    if (heap_size + size > HEAP_CAP) {
+        printf("Heap error: overflow occurred\n");
+        return NULL;
+    }
+
+    // Does not keep track of blocks
+    void *mem = heap_ptr;
+    heap_ptr += size;
+    heap_size += size;
+
+    return mem;
 
 }
 
 void heapc_free(void* ptr) {
-    
+
 } 
-
-
-
-
-
 
 
 
 int main() {
 
-    for (int i = 0; i < 10; i++) {
-        heap[i] = i; 
+
+    char *alpha = (char*)heapc_alloc(26*sizeof(char));
+
+    for (size_t i = 0; i < 26; i++) {
+        alpha[i] = 'A' + i;
     }
 
-    heap_dump();
+    // printf("\n%zu ", heap_ptr);
+
+    printf("Heap ptr at position: %d\n",heap_ptr-heap);
+    for (int i = 0; i < HEAP_CAP; i++) {
+        printf("%c", heap[i]);
+    }
 
     return 0;
 }
